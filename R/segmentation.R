b38e922884555eaef56cda3baf981aeaa4b7881c
shilpa_sasidharan <Optimal@5678>
2015-05-13 16:19:15 -0500

    new commit

--- R/segmentation.R
+++ R/segmentation.R
@@ -1,5 +1,19 @@
 getSegment <- function(answers) {
-  load("segment_fit.rda")
+  load("data/segment_fit.rda")
+  pred <- predict(segment_fit, newdata=answers, type="class")
+  
+  return(pred)
+}
+
+getSegment2 <- function(answers) {
+  load("../data/segment_fit.rda")
+  pred <- predict(segment_fit, newdata=answers, type="class")
+  
+  return(pred)
+}
+
+getSegment3 <- function(answers) {
+  load("../segment_fit.rda")
   pred <- predict(segment_fit, newdata=answers, type="class")
   
   return(pred)
