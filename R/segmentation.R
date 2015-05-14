127118dc15e973ed6ab52a85c4d3838c6d8e416c
shilpa_sasidharan <Optimal@5678>
2015-05-14 07:52:33 -0500

    fix again..

--- R/segmentation.R
+++ R/segmentation.R
@@ -1,26 +1,5 @@
 getSegment <- function(answers) {
-  load("data/segment_fit.rda")
-  pred <- predict(segment_fit, newdata=answers, type="class")
-  
-  return(pred)
-}
-
-getSegment2 <- function(answers) {
-  load("../data/segment_fit.rda")
-  pred <- predict(segment_fit, newdata=answers, type="class")
-  
-  return(pred)
-}
-
-getSegment3 <- function(answers) {
-  load("../segment_fit.rda")
-  pred <- predict(segment_fit, newdata=answers, type="class")
-  
-  return(pred)
-}
-
-getSegment4 <- function(answers) {
-  load("segment_fit.rda")
+  #load("data/segment_fit.rda")
   pred <- predict(segment_fit, newdata=answers, type="class")
   
   return(pred)
